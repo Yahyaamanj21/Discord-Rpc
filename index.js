752d4bfd987165243223ae4dde1b94cb70e1d22f
Yahyaamanj21 <81489379+Yahyaamanj21@users.noreply.github.com>
2021-05-03 16:56:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Your Text Here",
+details : "System Bot",
 assets : {
-large_image : "Image",
-large_text : "Your Status" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+large_image : "image1",
+large_text : "System Bot" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
-buttons : [{label : "Button1" , url : "Link1"},{label : "Button2",url : "Link2"}]
+buttons : [{label : "Add bot" , url : "https://discord.com/api/oauth2/authorize?client_id=826190816900808774&permissions=8&scope=bot"},{label : "Join My Server",url : "https://discord.gg/zq5KMfYEUa"}]
 }
 })
 })
-client.login({ clientId : "ApplicationID" }).catch(console.error);
+client.login({ clientId : "826190816900808774" }).catch(console.error);
